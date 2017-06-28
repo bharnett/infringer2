ee3277268086569e409de0d72c0e13244f579617
Brian Harnett <bharnett1825@gmail.com>
2017-06-28 09:49:45 -0400

    first files
    These are the first two files I've added

---
+++ Models.py
@@ -0,0 +1,237 @@
+import urllib
+import cherrypy
+from sqlalchemy import Column, String, Integer, ForeignKey, Date, Boolean, DateTime, create_engine
+from sqlalchemy.orm import relationship, backref, sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+from cherrypy.process import wspbus, plugins
+import os
+import re
+import datetime
+
+
+Base = declarative_base()
+
+
+class Show(Base):
+    __tablename__ = 'show'
+    show_id = Column(Integer, primary_key=True)
+    show_name = Column(String)
+    first_aired = Column(Date)
+    is_active = Column(Boolean, default=True)
+    banner = Column(String)
+    large_image = Column(String)
+    show_directory = Column(String)
+
+    def __str__(self):
+        return "%s - %s" % (self.show_name, self.show_id)
+
+
+class Episode(Base):
+    __tablename__ = 'episode'
+    id = Column(Integer, primary_key=True)
+    show_id = Column(Integer, ForeignKey('show.show_id'))
+    show = relationship(Show, backref=backref('episodes', cascade='delete', lazy='dynamic'))
+    season_number = Column(Integer)
+    episode_number = Column(Integer)
+    episode_name = Column(String)
+    air_date = Column(Date)
+    status = Column(String, default='Retrieved') # 'Pending', 'Retrieved'
+    attempts = Column(Integer, default=0)
+    retrieved_on = Column(Date)
+    episode_description = Column(String)
+    url_download_source = Column(String)
+    is_downloaded = Column(Boolean, default=False)
+    parent_download_page = Column(String)
+    download_time = Column(DateTime)
+
+
+    def __str__(self):
+        return "%s s%se%s" % (self.show.show_name, str(self.season_number).zfill(2), str(self.episode_number).zfill(2))
+
+    def get_episode_name(self):
+        return self.episode_name.replace('<', '').replace('>', '')
+
+
+class ScanURL(Base):
+    __tablename__ = 'scanurl'
+    id = Column(Integer, primary_key=True)
+    username = Column(String, default='myusername')
+    password = Column(String, default='mypassword')
+    login_page = Column(String, default='http://tehparadox.com/forum/')  # 'http://tehparadox.com/forum/'
+    url = Column(String, default='myurl')
+    media_type = Column(String, default='both')  # can be tv, movies, or both
+    priority = Column(Integer, nullable=True)
+    link_select = Column(String, default='a')
+    max_search_links = Column(Integer, default=300)
+    domain = Column(String, default='http://www.domain.com/')
+
+    def __str__(self):
+        return self.domain
+
+
+class Movie(Base):
+    __tablename__ = 'movie'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    link_text = Column(String)
+    status = Column(String, default='Not Retrieved')
+
+    def get_IMDB_link(self):
+        release_date_list = re.findall('[\(]?[0-9]{4}[\)]?', self.name)
+        # s = re.sub('[\(][0-9]{4}[\)]', '', self.name)
+        s = self.name.replace('(', '|').replace('Part', '|').replace('Season', '|')
+        s = s.split('|')[0].strip()
+        s = re.split('[\(]?[0-9]{4}[\)]?', s)[0].strip().replace(' ', '+')
+        if len(release_date_list) > 0:
+            release_date = release_date_list[0].replace('(', '').replace(')', '')
+        else:
+            release_date = ''
+        s = urllib.parse.quote_plus(s)
+        return 'http://www.omdbapi.com/?t=%s&y=%s&plot=short&r=json' % (s, release_date)
+
+
+class MovieURL(Base):
+    __tablename__ = 'movieurl'
+    id = Column(Integer, primary_key=True)
+    movie_id = Column(Integer, ForeignKey('movie.id'))
+    movie = relationship(Movie, backref=backref('movieurls', cascade='delete', lazy='dynamic'))
+    url = Column(String)
+
+
+class ActionLog(Base):
+    __tablename__ = 'actionlog'
+    id = Column(Integer, primary_key=True)
+    time_stamp = Column(DateTime)
+    message = Column(String)
+
+    def get_display(self):
+        return '%s -- %s' % (self.time_stamp, self.message)
+
+    @staticmethod
+    def log(msg):
+        # clean up the log file to keep it to the last 2000 records
+        s = connect()
+        l = ActionLog(time_stamp=datetime.datetime.now(), message=msg)
+        s.add(l)
+
+        all_logs = s.query(ActionLog).all()
+        if len(all_logs) == 3000:
+            entries_to_delete = all_logs[:2000]
+            for e in entries_to_delete:
+                s.delete(e)
+        s.commit()
+
+class LinkIndex(Base):
+    __tablename__ = "linkindex"
+    id = Column(Integer, primary_key=True)
+    link_text = Column(String)
+    link_url = Column(String)
+
+
+class Config(Base):
+    __tablename__ = 'config'
+    id = Column(Integer, primary_key=True)
+    crawljob_directory = Column(String, default='')
+    tv_parent_directory = Column(String, default='')
+    movies_directory = Column(String, default='')
+    file_host_domain = Column(String, default='uploaded.net')  # 'uploaded.net'
+    hd_format = Column(String, default='720p')  # only 720p or 1080p
+    ip = Column(String, default='127.0.0.1')
+    port = Column(String, default='8080')
+    scan_interval = Column(Integer, default=12)
+    refresh_day = Column(String, default='sun')
+    refresh_hour = Column(Integer, default=2)
+    jd_link = Column(String, default='')
+    jd_path = Column(String, default='')
+
+    @staticmethod
+    def get_hours():
+        return list(range(1, 25))
+
+    @staticmethod
+    def get_intervals():
+        return list(range(2, 13))
+
+    def is_populated(self):
+        if not self.crawljob_directory and not self.tv_parent_directory and not self.movies_directory:
+            return False
+        else:
+            return True
+
+    def domain_link_check(self, link):
+        domains = self.file_host_domain.split(',')
+        file_host_exists = False
+        for d in domains:
+            if d.strip() in link:
+                file_host_exists = True
+                break
+        return file_host_exists
+
+
+        # http://docs.sqlalchemy.org/en/rel_0_9/dialects/sqlite.html
+
+
+def connect():
+    data_file = os.path.normpath(os.path.abspath(__file__))
+    print(data_file)
+    data_dir = os.path.dirname(data_file)
+    db_path = data_dir + '/db.sqlite3'
+    print('DB Path: %s' % db_path)
+    # engine = create_engine('sqlite:///db.sqlite3', connect_args={'check_same_thread':False})
+    engine = create_engine('sqlite:///%s' % db_path, echo=True)
+    session_factory = sessionmaker()
+    session_factory.configure(bind=engine)
+    Base.metadata.create_all(engine)
+    s = scoped_session(session_factory)
+    return s
+
+
+# http://www.defuze.org/archives/222-integrating-sqlalchemy-into-a-cherrypy-application.html
+class SAEnginePlugin(plugins.SimplePlugin):
+    def __init__(self, bus):
+        plugins.SimplePlugin.__init__(self, bus)
+        self.sa_engine = None
+        self.bus.subscribe("bind", self.bind)
+
+    def start(self):
+        data_file = os.path.normpath(os.path.abspath(__file__))
+        print(data_file)
+        data_dir = os.path.dirname(data_file)
+        db_path = data_dir + '/db.sqlite3'
+        print('DB Path: %s' % db_path)
+        self.sa_engine = create_engine('sqlite:///%s' % db_path, echo=True)
+        Base.metadata.create_all(self.sa_engine)
+
+    def stop(self):
+        if self.sa_engine:
+            self.sa_engine.dispose()
+            self.sa_engine = None
+
+    def bind(self, session):
+        session.configure(bind=self.sa_engine)
+
+
+class SATool(cherrypy.Tool):
+    def __init__(self):
+        cherrypy.Tool.__init__(self, 'on_start_resource',
+                               self.bind_session,
+                               priority=20)
+        self.session = scoped_session(sessionmaker(autoflush=True, autocommit=False))
+
+    def _setup(self):
+        cherrypy.Tool._setup(self)
+        cherrypy.request.hooks.attach('on_end_resource', self.commit_transaction, priority=80)
+
+    def bind_session(self):
+        cherrypy.engine.publish('bind', self.session)
+        cherrypy.request.db = self.session
+
+    def commit_transaction(self):
+        cherrypy.request.db = None
+        try:
+            self.session.commit()
+        except:
+            self.session.rollback()
+            raise
+        finally:
+            self.session.remove()
---
+++ Searcher.py
@@ -0,0 +1,83 @@
+import os
+import re
+import datetime
+
+"""this module holds the class and functions for searching episodes"""
+
+class Searcher(object):
+    """class for searching for an individual episode"""
+    def __init__(self, episode_id, episode_code, link, directory, attempts=0):
+        self.episode_id = episode_id
+        self.episode_code = episode_code
+        self.link = link
+        self.found = False
+        self.directory = directory
+        self.retrieved = False
+        self.search_list = []
+        self.attempts = attempts
+
+    def __str__(self):
+        return '%s %s' % (self.search_list[0], self.episode_code)
+
+    @staticmethod
+    def populate_searcher(episodes, config):
+        search_episodes = []
+        for e in episodes:
+            search_episodes.append(Searcher.populate_episode(e, config.tv_parent_directory))
+
+        return search_episodes
+
+    @staticmethod
+    def populate_episode(episode, parent_dir):
+        hide_chars = [('e', '3'), ('l', '1'), ('o', '0')]
+        # these are the characters that need to be replaced
+        # this is only for the first instance of the letter in each word
+        edit_chars = [('', ''), ('.', ' '), ('.', ''), ('&', 'and')]
+         # first one handles initial non-char-edited name
+        second_chars = [('\'', '')]
+        # remove dates from show names (2014), (2009) for accurate string searches
+        edited_show_name = re.sub(r'[\(][0-9]{4}[\)]', '',
+                                  episode.show.show_name)
+        episode_id_string = 's%se%s' % (str(episode.season_number).zfill(2),
+                                        str(episode.episode_number).zfill(2))
+        show_dir = parent_dir + episode.show.show_directory
+        if not os.path.exists(show_dir):
+            os.makedirs(show_dir)
+
+        search_episode = Searcher(episode.id, episode_id_string, '', show_dir, episode.attempts)
+
+        for char in edit_chars:
+            char_edit_name = edited_show_name.replace(char[0], char[1]).strip().lower()
+            search_episode.search_list.append(char_edit_name)
+            for second in second_chars:
+                edit_name = char_edit_name.replace(second[0], second[1]).strip().lower()
+                search_episode.search_list.append(edit_name)
+
+        # remove duplicates
+        search_episode.search_list = list(set(search_episode.search_list))
+        return search_episode
+
+    def search_me(self, link_text):
+        is_found = False
+        if self.episode_code in link_text:
+            for s in self.search_list:
+                if s in link_text:
+                    is_found = True
+                    return is_found
+                else:
+                    continue
+        return is_found
+
+    @staticmethod
+    def list_completed(list_of_shows):
+        items = [item for item in list_of_shows if item.found is False]
+        return len(items) == 0
+    
+    def make_hidden_char_names(self, hide_chars, episode):
+        """get variants that have hidden characters"""
+        temp_name = episode.show.show_name
+        show_words = temp_name.split(' ')
+        for word in show_words:
+                for char in hide_chars:
+                    
+        
